73dc8c82f872ecafb6da8309616d1da7639120b1
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2023-06-11 22:27:14 +0700

    test to hash password with bcrypt

--- test/example_test.go
+++ test/example_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"testing"
 	"time"
 )
@@ -9,4 +10,37 @@ import (
 func TestExample(t *testing.T) {
 	now := time.Now()
 	fmt.Println("Unix Format", now.Format("2006-01-02"))
+}
+
+func TestHashPassword(t *testing.T) {
+	password := "password"
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		panic(err)
+	}
+
+	hashedPasswordString := string(hashPassword)
+
+	fmt.Println(hashedPasswordString)
+}
+
+func TestCompareHashPassword(t *testing.T) {
+	passwordInDB := "password"
+	requestPassword := "password123"
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(passwordInDB), bcrypt.DefaultCost)
+
+	if err != nil {
+		panic(err)
+	}
+	hashedPasswordString := string(hashPassword)
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPasswordString), []byte(requestPassword))
+
+	if err != nil {
+		fmt.Println("Password is missmatch")
+	} else {
+		fmt.Println("Password is match")
+	}
+
 }
