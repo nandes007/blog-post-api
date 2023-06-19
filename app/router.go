3b2e65c672743c9a75665505ea21bebeb8c7068a
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2023-06-19 21:25:33 +0700

    add middleware authentication

--- app/router.go
+++ app/router.go
@@ -4,12 +4,14 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"nandes007/blog-post-rest-api/controller"
 	"nandes007/blog-post-rest-api/exception"
+	"nandes007/blog-post-rest-api/middleware"
 )
 
 func NewRouter(userController controller.UserController) *httprouter.Router {
 	router := httprouter.New()
 	router.POST("/api/users", userController.Create)
-	router.GET("/api/users", userController.FindAll)
+	//router.GET("/api/users", userController.FindAll)
+	router.GET("/api/users", middleware.JwtAuthMiddleware(userController.FindAll))
 	router.POST("/api/users/login", userController.Login)
 
 	router.PanicHandler = exception.ErrorHandler
--- middleware/jwt_middleware.go
+++ middleware/jwt_middleware.go
@@ -1,1 +1,44 @@
 package middleware
+
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/julienschmidt/httprouter"
+	"nandes007/blog-post-rest-api/helper"
+	"nandes007/blog-post-rest-api/model/web"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func JwtAuthMiddleware(next httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		tokenSigning := os.Getenv("JWT_SIGNING")
+		bearerToken := r.Header.Get("Authorization")
+		tokenString := strings.Replace(bearerToken, "Bearer ", "", 1)
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+
+			return []byte(tokenSigning), nil
+		})
+
+		if err != nil || !token.Valid {
+			//w.WriteHeader(http.StatusUnauthorized)
+			//w.Write([]byte("Unauthorized"))
+			//return
+			apiResponse := web.ApiResponse{
+				Code:   401,
+				Status: "Failed",
+				Data:   "Unauthorized",
+			}
+
+			helper.WriteToResponseBody(w, apiResponse)
+			return
+		}
+
+		next(w, r, ps)
+	}
+}
