241862f893250d01fba0b9644643f8c51848d74d
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2024-02-21 08:51:59 +0700

    add auth unit test success case

--- app/test/auth_test.go
+++ app/test/auth_test.go
@@ -2,7 +2,11 @@ package test
 
 import (
 	"nandes007/blog-post-rest-api/model/web/auth"
+	"nandes007/blog-post-rest-api/service"
+	"testing"
 
+	"github.com/go-playground/validator/v10"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -18,4 +22,50 @@ func (m *mockAuthRepository) Login(req *auth.LoginRequest) (*auth.LoginResponse,
 func (m *mockAuthRepository) Register(req *auth.RegisterRequest) (*auth.RegisterResponse, error) {
 	args := m.Called(req)
 	return args.Get(0).(*auth.RegisterResponse), args.Error(1)
+}
+
+func TestAuthService_Login(t *testing.T) {
+	mockRepo := &mockAuthRepository{}
+	validate := validator.New()
+	service := service.NewAuthService(mockRepo, validate)
+
+	expected := &auth.LoginResponse{
+		Token: "asdadasdasdasdadsasdasd",
+	}
+
+	req := &auth.LoginRequest{
+		Email:    "test@example.com",
+		Password: "password",
+	}
+
+	mockRepo.On("Login", req).Return(expected, nil)
+	token, err := service.Login(req)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, token)
+	mockRepo.AssertExpectations(t)
+}
+
+func TestAuthService_Register(t *testing.T) {
+	mockRepo := &mockAuthRepository{}
+	validate := validator.New()
+	service := service.NewAuthService(mockRepo, validate)
+
+	expected := &auth.RegisterResponse{
+		ID:    1,
+		Name:  "test",
+		Email: "test@example.com",
+	}
+
+	req := &auth.RegisterRequest{
+		Name:     "test",
+		Email:    "test@example.com",
+		Password: "password",
+	}
+
+	mockRepo.On("Register", req).Return(expected, nil)
+
+	res, err := service.Register(req)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, res)
+	mockRepo.AssertExpectations(t)
 }
